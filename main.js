2a9f8e288278e15872362ebe513d2f35217fe0fd
weronieckim <weronieckim@gmail.com>
2020-03-11 17:07:37 +0100

    CRD without U

--- main.js
+++ main.js
@@ -1,38 +1,77 @@
 var MongoClient = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
 var url = "mongodb+srv://Kanban-User:ZAQ%212wsx@kanban-arkwo.mongodb.net/test?retryWrites=true&w=majority";
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const path = require('path');
 const router = express.Router();
+var assert = require('assert');
+var exphbs  = require('express-handlebars');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+//const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname+'/index.html'));
-});
+app.engine('.hbs', exphbs({extname: '.hbs'}));
+app.set('view engine', '.hbs');
 
-app.use(express.static(__dirname + '/View'));
+router.get('/', (req, res, next) => {
+    res.render('index');
+  });
 
-app.post('/add', async (req, res) => {
-    client.connect(function(err, db) {
-        if (err) throw err;
+
+router.post('/create', async (req, res) => {
+    var myobj = { 
+        name: req.body.name,
+        description: req.body.description,
+        status: req.body.status
+     };
+     MongoClient.connect(url, { useUnifiedTopology: true, useNewUrlParser: true, }, (err, db) => {
+        assert.equal(null, err);
         var dbo = db.db("WorkersDB");
-        var myobj = { 
-            name: req.body.name,
-            description: req.body.description,
-            status: req.body.status
-         };
-        dbo.collection("Tasks").insertOne(myobj, function(err, res) {
-          if (err) throw err;
-          console.log("1 document inserted");
-          db.close();
+        dbo.collection("Tasks").insertOne(myobj, (err, res) => {
+            assert.equal(null, err);
+            console.log("Item inserted");
+            db.close();
         });
         res.redirect('/');
       });
+});
+
+router.get('/read', async (req, res) => {
+    var resultArray = [];
+    MongoClient.connect(url, { useUnifiedTopology: true, useNewUrlParser: true, }, (err, db) => {
+        assert.equal(null, err);
+        var dbo = db.db("WorkersDB");
+        var cursor = dbo.collection('Tasks').find();
+        cursor.forEach((doc, err) => {
+            assert.equal(null, err);
+            resultArray.push(doc);
+        },
+        () => {
+            db.close();
+            res.render('index', {items: resultArray});
+        });
+    });
+});
+
+router.get('/update/:id', async (req, res) => {
+
+})
+
+
+router.get('/delete/:id', async (req, res) => {
+    MongoClient.connect(url, { useUnifiedTopology: true, useNewUrlParser: true, }, (err, db) => {
+        assert.equal(null, err);
+        var dbo = db.db("WorkersDB");
+        dbo.collection("Tasks").deleteOne({ "_id": objectId(req.params.id)}, (err, result) => {
+            assert.equal(null, err);
+            console.log("Object with _id: "+req.params.id+" deleted successfully");
+            db.close();
+            res.redirect("/read");
+        });
+    });
 });
 
 
@@ -46,63 +85,4 @@ app.listen(process.env.port || 3000);
 
 console.log('Running at Port 3000');
 
-
-async function deleteTaskByName(client, nameOfTask){
-    const result = await client.db("WorkersDB").collection("Tasks")
-    .deleteOne({name: nameOfTask});
-    console.log(`${result.deletedCount} jebanych taskow sie usunelo`);
-}
-
-
-async function updateTaskByName(client, nameOfTask, updatedTask){
-    const result = await client.db("WorkersDB").collection("Tasks").updateOne(
-        { name: nameOfTask},
-        { $set: updatedTask}
-     );
-
-     console.log(`${result.matchedCount} document(s) matched the query criteria`);
-     console.log(`${result.modifiedCount} document(s) updated`);
-}
-
-
-async function findTasksByStatus(client, { statusName }){
-    const cursor = await client.db("WorkersDB").collection("Tasks").find({
-        status: statusName
-    })
-
-    const results = await cursor.toArray();
-
-    if(results.length > 0) {
-        console.log(`Znaleziono`);
-        results.forEach((result, i) => {
-            console.log();
-            console.log(`${i+1}. name: ${result.name}`);
-            console.log(`_id: ${result._id}`);
-            console.log(`description: ${result.description}`);
-            console.log(`status: ${result.status}`);
-        });
-    }
-}
- 
-
-
-async function findOneTaskByName(client, nameOfTask){
-    const result = await client.db("WorkersDB").collection("Tasks").findOne({ name: nameOfTask });
-    if(result) {
-        console.log(`Znaneziono jebanego taska z jebana nazwa '${nameOfTask}':`);
-        console.log(result);
-    } else {
-        console.log(`Nie znleziono jebanej nazwy: '${nameOfTask}'`);
-    }
-}
-
-async function createMultipleTasks(client, newTasks){
-    const result = await client.db("WorkersDB").collection("Tasks").insertMany(newTasks);
-    console.log(`${result.insertedCount} nowe KURWA rekordy zostaly dodane z jebanymi ID:`);
-    console.log(result.insertedIds);
-};
-
-function createTask(client, newTask){
-    const result = client.db("WorkersDB").collection("Tasks").insertOne(newTask);
-    console.log(`Jebane ID nowego taska: ${result.insertedId}`);
-};
+module.exports = router;
